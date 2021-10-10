d5ed412730bab6efb42a571eec42b77cafcf58da
DragnEmperor <dragnemperor@gmail.com>
2021-10-10 23:58:11 +0530

    Added Imports and general layout
    Signed-off-by: DragnEmperor <dragnemperor@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import Header from './components/UI/Header/Header' 
-import Footer from './components/UI/Footer/footer' 
-import Container from './components/UI/Container/Container' 
+import Header from './components/UI/Header/Header';
+import Footer from './components/UI/Footer/footer';
+import Intro from './components/UI/Intro/Intro';
+import Content from './components/UI/Content/Content';
+import About from './components/About/About';
+import FAQ from './components/FAQs/faq';
+import Judges from './components/Judges/judges';
+import Milestone from './components/Milestone/milestone';
+import Prizes from './components/Prizes/prizes';
+import ProbStatements from './components/Prob_statements/problems';
+import Projects from './components/Projects/Projects';
+import Schedule from './components/Schedule/schedule';
+import Sponsors from './components/Sponsors/Sponsors';
+import Workshop from './components/Workshop/Workshop';
 
 
 const App=()=>{
   return(
     <React.Fragment>
-     <div className='bg-indigo-300 h-96 w-full'>
-      
-     </div>
+      <Header/>
+      <Intro/>
+      <About/>
+      <Content/>
+      <Milestone/>
+      <Schedule/>
+      <Projects/>
+      <ProbStatements/>
+      <Prizes/>
+      <Workshop/>
+      <Judges/>
+      <Sponsors/>
+      <FAQ/>
+      <Footer/>
+
     </React.Fragment>
   )
 }
---
+++ src/components/UI/Content/Content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Content() {
+    return (
+        <div>
+            <h1>Content</h1>
+        </div>
+    )
+}
