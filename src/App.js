b323cdc15760d417966d4866fd46357ab7e35bea
DragnEmperor <dragnemperor@gmail.com>
2021-10-10 23:10:55 +0530

    Added Directory Structure
    Signed-off-by: DragnEmperor <dragnemperor@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/UI/Header/Header' 
+import Footer from './components/UI/Footer/footer' 
+import Container from './components/UI/Container/Container' 
+
+
+const App=()=>{
+  return(
+    <React.Fragment>
+     <div className='bg-indigo-300 h-96 w-full'>
+      
+     </div>
+    </React.Fragment>
+  )
+}
+
+export default App;
