2f39093745cf1335f4ff38a4be113470a4704a50
Avishrant Sharma <avishrants@gmail.com>
2021-10-12 00:56:44 +0530

    Add sticky contact section
    Signed-off-by: Avishrant Sharma <avishrants@gmail.com>

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Projects from "./components/Themes/Themes";
 import Schedule from "./components/Schedule/schedule";
 // import Sponsors from "./components/Sponsors/Sponsors";
 // import Workshop from "./components/Workshop/Workshop";
+import Contact from "./components/Contact/Contact";
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init({
@@ -24,13 +25,14 @@ const App = () => {
     <React.Fragment>
       <Header />
       <Intro />
+      <Contact />
       <div className="container mx-auto px-4">
         <About />
         <Content />
         <Milestone />
         <Schedule />
         <Projects />
-        <ProbStatements />
+        {/* <ProbStatements /> */}
         {/* <Prizes /> */}
         {/* <Workshop /> */}
         {/* <Judges /> */}
--- src/components/Themes/Themes.js
+++ src/components/Themes/Themes.js
@@ -1,28 +1,29 @@
 import React from "react";
-import './themes.css';
-import themeArray from './themesdata.json';
+import "./themes.css";
+import themeArray from "./themesdata.json";
 
 export default function Projects() {
-  return (<section id='problems' class="problem_body">
-    <div class="problemContainer">
-      <ul class="cards">
-        {themeArray.map(e=>{
-        return(<li class="card cards__item">
-          <div class="card__frame">
-            <div class="card__picture">
-            </div>
-            <h2 class="card__title">{e.title}</h2>
-          </div>
-          <div class="card__overlay"></div>
-          <div class="card__content">
-            <h2>{e.title}</h2>
-            <p>{e.content}</p>
-          </div>
-        </li>)
-        })}
-      </ul>
-    </div>
-  </section>
+  return (
+    <section id="problems" class="problem_body">
+      <div class="problemContainer">
+        <ul class="cards">
+          {themeArray.map((e) => {
+            return (
+              <li class="card cards__item">
+                <div class="card__frame">
+                  <div class="card__picture"></div>
+                  <h2 class="card__title">{e.title}</h2>
+                </div>
+                <div class="card__overlay"></div>
+                <div class="card__content">
+                  <h2>{e.title}</h2>
+                  <p>{e.content}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
   );
 }
-
