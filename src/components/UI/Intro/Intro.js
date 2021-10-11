7ac61d0e967e9b573bcfece54eb6be81febb118e
Avishrant Sharma <avishrants@gmail.com>
2021-10-11 11:10:28 +0530

    Update Hero Section
    Signed-off-by: Avishrant Sharma <avishrants@gmail.com>

--- src/components/UI/Intro/Intro.js
+++ src/components/UI/Intro/Intro.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import BgImage from './bg.jpg'
 
-export default function Container() {
+export default function Introduction() {
     return (
-        <div>
-            <h1>container</h1>
+        <div className="relative bg-white overflow-hidden">
+            <div className="max-w-7xl mx-auto">
+                <div className="relative pb-8 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32 lg:max-w-2xl lg:w-full">
+                    <main className="mt-10 mx-auto px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 lg:py-8 xl:mt-28">
+                        <div className="sm:text-center lg:text-left">
+                            <h1 className="text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
+                                <span className="block xl:inline">Electothon 4.0</span>
+                                <span className="block text-indigo-600">2022</span>
+                            </h1>
+                            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                SEARCH | PLAN | ENGAGE | CREATE
+                            </p>
+                            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                                <div className="rounded-md shadow">
+                                    <a
+                                        href="#"
+                                        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10 disabled"
+                                    >
+                                        Apply with Devfolio
+                                    </a>
+                                </div>
+                                <div className="mt-3 sm:mt-0 sm:ml-3">
+                                    <a
+                                        href="#"
+                                        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                                    >
+                                        Electrothon 3.0
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                </div>
+            </div>
+            <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                <img
+                    className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full hero-background"
+                    src={BgImage}
+                    alt=""
+                />
+            </div>
         </div>
     )
 }
