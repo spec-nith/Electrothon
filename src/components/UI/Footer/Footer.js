556615f1fb601d8af79f008c14a98218658fdc24
Smriti <20bcs105@nith.ac.in>
2022-01-12 13:42:28 +0530

    Footer layout changed.

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -1,40 +1,111 @@
 import React from "react";
 import { useForm } from "@formspree/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiscord, faFacebook, faTwitter, faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
   const BottomFooter = () => {
     return (
-      <div className="flex flex-col md:flex-row items-center justify-around mt-6">
-        <div className="flex invisible md:visible">
+
+      <div className="md:flex justify-center gap-5 lg:gap-40 mt-5">
+        <div className=" md:visible flex justify-center mb-2">
           <a
             href="https://specnith.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-sm md:text-lg md:font-bold text-white hover:text-indigo-400"
+            className="text-sm md:text-lg md:font-bold text-white hover:text-gray-300 "
           >
-            <img className="h-28" src="logo.png" alt="spec_logo" />
+            <img className="h-10 md:h-28" src="logo.png" alt="spec_logo" />
           </a>
         </div>
-        <div className="flex">
-          <a
-            href="https://drive.google.com/file/d/10KVmRYHKAQIkMVR3nFIqbaPpL1wpMYTz/view?usp=drivesdk"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sm md:text-lg md:font-bold text-white hover:text-indigo-400"
-          >
-            CODE OF CONDUCT
-          </a>
+        <div className=" flex justify-center mb-4">
+          <ul >
+            <li className=" text-gray-500 font-bold text-sm md:text-lg flex justify-center md:justify-start">ABOUT</li>
+            <li>
+              <a
+                href="mailto:spec@nith.ac.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex justify-center md:justify-start text-sm md:md:text-sm  text-white hover:text-gray-300"
+              >
+                Mail to: spec@nith.ac.in
+              </a>
+              <li> <a
+                href="https://drive.google.com/file/d/1P_-npMEWQtOUAWnuJH66aK5h7sCxSgRy/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex justify-center md:justify-start text-sm md:md:text-sm text-white hover:text-gray-300"
+              >
+                Sponsorship Brochure
+              </a></li></li>
+            <li>
+              <a
+                href="https://drive.google.com/file/d/10KVmRYHKAQIkMVR3nFIqbaPpL1wpMYTz/view?usp=drivesdk"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex justify-center md:justify-start text-sm md:md:text-sm text-white hover:text-gray-300"
+              >
+                Code of Conduct
+              </a> </li>
+            <li>
+              <a
+                href="https://www.notion.so/Hacker-s-Guide-to-Electrothon-3-0-528b6b2f10bd48dd812ed8dd9f99ce7f"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex justify-center md:justify-start text-sm md:md:text-sm text-white hover:text-gray-300"
+              >
+                Hackers Guide
+              </a></li>
+          </ul>
         </div>
-        <div className="flex">
-          <a
-            href="https://drive.google.com/file/d/1P_-npMEWQtOUAWnuJH66aK5h7sCxSgRy/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sm md:text-lg md:font-bold text-white hover:text-indigo-400"
-          >
-            SPONSORSHIP BROCHURE
-          </a>
+        <div className="text-white flex text-sm md:text-lg justify-center mb-4 ">
+          <ul>
+            <li className="text-gray-500 font-bold flex justify-center md:justify-start">ADDRESS</li>
+            <li className=" md:text-sm flex justify-center md:justify-start">NIT-Hamirpur</li>
+            <li className=" md:text-sm flex justify-center md:justify-start">Himachal Pradesh, India</li>
+            <li className=" md:text-sm flex justify-center md:justify-start">177005</li>
+          </ul>
+
+        </div>
+        <div class="">
+          <div className="flex justify-center md:justify-start text-sm md:text-lg font-bold  text-gray-500">
+            STAY CONNECTED</div>
+
+          <div className="justify-center flex space-x-2 md:space-x-5 mt-1 md:mt-1 opacity-80 mb-10">
+            <a
+              href="https://discord.gg/cjMMRs7tHs"
+              rel="noreferrer noopenor"
+              target="_blank">
+              <FontAwesomeIcon className="text-1xl md:text-2xl text-blue-800 fill-current" icon={faDiscord} />
+            </a>
+            <a
+              href="https://www.facebook.com/spec.ece/"
+              rel="noreferrer noopenor"
+              target="_blank">
+              <FontAwesomeIcon className="text-1xl md:text-2xl text-blue-600 fill-current" icon={faFacebook} />
+            </a>
+            <a
+              href="https://twitter.com/SPEC__NITH"
+              rel="noreferrer noopenor"
+              target="_blank">
+              <FontAwesomeIcon className="text-1xl md:text-2xl text-blue-300 fill-current" icon={faTwitter} />
+            </a>
+            <a
+              href="https://instagram.com/s.p.e.c_nith?utm_source=ig_profile_share&amp;igshid=1dd01jvv7xk83"
+              rel="noreferrer noopenor"
+              target="_blank">
+              <FontAwesomeIcon className="text-1xl md:text-2xl text-pink-600 fill-current" icon={faInstagram} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/s-p-e-c-nith-40214b197/"
+              rel="noreferrer noopenor"
+              target="_blank">
+              <FontAwesomeIcon className="text-1xl md:text-2xl text-blue-500 fill-current" icon={faLinkedin} />
+            </a>
+          </div>
+
         </div>
       </div>
+
     );
   };
 
@@ -53,10 +124,10 @@ export default function Footer() {
                 <h2 className="text-4xl lg:text-5xl font-bold leading-tight header-theme">
                   Lets talk about everything!
                 </h2>
-                <div className="text-gray-200 mt-8">
+                <div className="text-gray-300 mt-8">
                   Hate forms? Send us an{" "}
                   <a
-                    className="underline hover:text-indigo-400"
+                    className="underline hover:text-gray-300"
                     href="mailto:spec@nith.ac.in"
                     target="_blank"
                     rel="noopener noreferrer"
@@ -106,7 +177,7 @@ export default function Footer() {
               <h2 className="text-4xl lg:text-5xl font-bold leading-tight header-theme">
                 Lets talk about everything!
               </h2>
-              <div className="text-gray-200 mt-8">
+              <div className="text-gray-300 mt-8">
                 Hate forms? Send us an{" "}
                 <a
                   className="underline"
