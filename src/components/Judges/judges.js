16baa537fbc5ed0e21438a50bcd92fa331acd43f
suyash2002 <suyashbarwal@gmail.com>
2022-01-20 21:17:16 +0530

    Made Judges Section

--- src/components/Judges/judges.js
+++ src/components/Judges/judges.js
@@ -1,6 +1,6 @@
 import React from "react";
 import JudgesCard from "./judgesCard";
-import { judgesData } from "./judgesData";
+import judgesData from "./judgesData.json";
 
 export default function judges() {
   return (
@@ -12,22 +12,17 @@ export default function judges() {
       data-aos-duration="1000"
       data-aos-easing="ease-in-out-cubic"
     >
-      <h1 className="text-3xl tracking-tight md:text-4xl leading-normal md:leading-relaxed header-theme">
+      <h1 className="text-3xl tracking-tight md:text-4xl leading-normal md:leading-relaxed header-theme flex justify-center">
         Judges
       </h1>
       <div className="mt-5 md:mt-0">
-        {/* {judgesData.map((element, index) => {
-                return (
-                    <JudgesCard key={index} judge={element} />
-                )
-            })} */}
-        <section className="container mx-auto p-10 md:py-20 px-0 md:p-20 md:px-0 antialiased">
-          <section className="grid lg:grid-cols-2 2xl:grid-cols-5 grid-cols-1 gap-20">
+        <div className="container mx-auto mt-8">
+          <div className="flex flex-row flex-wrap -mx-2 justify-center">
             {judgesData.map((element, index) => {
               return <JudgesCard key={index} judge={element} />;
             })}
-          </section>
-        </section>
+          </div>
+        </div>
       </div>
     </section>
   );
