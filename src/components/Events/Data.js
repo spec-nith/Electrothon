db20f2abdf8668b5892e1f51d47ed80c0e965169
mitabhay <leader.abhay002@gmail.com>
2023-03-21 16:57:55 +0530

    Orbit of Open-Source Event Image changed

--- src/components/Events/Data.js
+++ src/components/Events/Data.js
@@ -3,7 +3,7 @@ export const PreviousEvents = [];
 export const LiveEvents = [
   {
     name: "Orbit of Open Source",
-    img: "Events/orbit of open source.png",
+    img: "Events/orbit of open source.jpeg",
     desc: "Team SPEC has returned to spread the word about the power of open source software throughout the cosmos. Just as stars in a constellation work together to create something beautiful, the open source community collaborates to bring innovation to the far reaches of the galaxy.🪐🪐So prepare for liftoff, because Team SPEC is taking you on a journey through the stars to explore the world of open source at BoardRoom for Hackers 2.0: Orbit of Open-Source. Let's boldly go where no hacker has gone before!🪩🪩",
     webp: "Events/webp/orbit of open source.webp",
     link: "https://spec.nith.ac.in/OrbitofOpenSource/index.html",
