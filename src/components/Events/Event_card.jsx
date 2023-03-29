cae5db7a82c6022fd5688f6711c4a47d800778e1
priyanshuRj <priyanshurajput0071109@gmail.com>
2023-03-29 18:55:49 +0530

    Event Card Glitch Resolved

--- src/components/Events/Event_card.jsx
+++ src/components/Events/Event_card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 
 const sliderSettings = {
@@ -41,18 +41,21 @@ const sliderSettings = {
 export default function EventCard(props) {
   const [sliderRef, setSliderRef] = useState(null);
   const [currentSlide, setCurrentSlide] = useState(0);
-
+  useEffect(()=>{
+    setCurrentSlide(0);
+    console.log("called")
+  },[props])
   // console.log(props.data, currentSlide);
   return (
     <div className="w-full">
       {props.data.length !== 0 ? (
-        <div className="md:flex justify-center space-x-2">
+        <div className="justify-center space-x-2 md:flex">
           <div className="mx-auto md:w-[50%] w-full text-white p-4 ">
             <Slider
               ref={setSliderRef}
               {...sliderSettings}
               afterChange={async (index) => {
-                await setCurrentSlide(index);
+                setCurrentSlide(index);
               }}
             >
               {props.data.map((event, index) => (
@@ -73,11 +76,11 @@ export default function EventCard(props) {
           </div>
           <div className="text-white hidden md:flex md:justify-center items-center w-[50%]  bg-gradient-to-br from-[rgba(209,209,209,0.2)] to-[rgba(209,209,209,0.1)] backdrop-blur-[5px] rounded-3xl lg:h-auto md:h-[600px]">
             <div className="flex flex-col justify-evenly p-8 space-y-8 h-[90%]">
-              <div className="text-center p-2 text-4xl font-bold">
-                {props.data[currentSlide].name}
+              <div className="p-2 text-4xl font-bold text-center">
+                {props.data[currentSlide] ? props.data[currentSlide].name : ""}
               </div>
               <div className=" h-[60%] overflow-y-scroll align-middle text-center text-lg">
-                {props.data[currentSlide].desc}
+                {props.data[currentSlide] ? props.data[currentSlide].desc : ""}
               </div>
 
               {props.title !== "Previous" ? (
@@ -86,7 +89,7 @@ export default function EventCard(props) {
                   className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-xs md:text-sm px-5 py-2.5 text-center mr-2 mb-2 "
                 >
                   <a
-                    href={props.data[currentSlide].link}
+                    href={props.data[currentSlide] ? props.data[currentSlide].link : ""}
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -97,13 +100,13 @@ export default function EventCard(props) {
             </div>
           </div>
           {props.title !== "Previous" ? (
-            <div className="md:hidden text-center mt-6">
+            <div className="mt-6 text-center md:hidden">
               <button
                 type="button"
                 className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-md  px-10 py-2.5 text-center  "
               >
                 <a
-                  href={props.data[currentSlide].link}
+                  href={props.data[currentSlide] ? props.data[currentSlide].link : ""}
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -114,7 +117,7 @@ export default function EventCard(props) {
           ) : null}
         </div>
       ) : (
-        <div className="text-xl text-white text-center">
+        <div className="text-xl text-center text-white">
           No {props.title} Events Found
         </div>
       )}
